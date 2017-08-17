98261ed58cac74b73a375dd5e1184f8f8cb09196
Mykola Shevchuk <30670820+shevchukms@users.noreply.github.com>
2017-08-17 14:53:01 +0300

    q

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import Flower.*;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Flower flower=new Flower(true,5, new FlowerSpec(FlowerColor.GREEN,1,FlowerType.MAK));
+
+
+
+
+    }
+}
